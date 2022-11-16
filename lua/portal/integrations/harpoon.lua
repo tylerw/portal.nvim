d3be8068c17724bea33fff3c9420a36cab23cca3
Calvin Bochulak <cbochula@cisco.com>
2022-11-16 07:09:59 -0700

    fix(harpoon): return boolean in early exit + require the right module

--- lua/portal/integrations/harpoon.lua
+++ lua/portal/integrations/harpoon.lua
@@ -5,15 +5,16 @@ local M = {}
 local function is_marked(jump)
     local query = require("portal.query")
     if not query.valid(jump) then
-        return
+        return false
     end
+
     local buffer_name = require("harpoon.utils").normalize_path(vim.api.nvim_buf_get_name(jump.buffer))
     local mark = require("harpoon.mark").get_marked_file(buffer_name)
     return query.different(jump) and mark ~= nil
 end
 
 function M.register()
-    local ok, _ = pcall(require, "grapple")
+    local ok, _ = pcall(require, "harpoon")
     if not ok then
         require("portal.log").warn("Unable to register query item. Please check that harpoon is installed.")
         return
